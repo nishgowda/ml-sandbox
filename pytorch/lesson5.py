8e1411e58b97f6fe09ecf2a6e455153647c9a9e1
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2020-06-21 20:32:45 -0400

    Training Convent

--- pytorch/lesson5.py
+++ pytorch/lesson5.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 REBUILD_DATA = False
 
@@ -40,8 +44,83 @@ if REBUILD_DATA:
 	dogvcats.make_training_data()
 
 training_data = np.load("training_data.npy", allow_pickle=True)
-print(len(training_data))
-print(training_data[1])
-plt.imshow(training_data[1][0], cmap = "gray")
-plt.show()
+#print(len(training_data))
+#print(training_data[1])
+#plt.imshow(training_data[1][0], cmap = "gray")
+#plt.show()
 
+
+class Net(nn.Module):
+        def __init__(self):
+                super().__init__()
+                self.conv1 = nn.Conv2d(1, 32, 5)
+                self.conv2 = nn.Conv2d(32, 64, 5)
+                self.conv3 = nn.Conv2d(64, 128, 5)
+                
+                x = torch.randn(50,50).view(-1,1,50,50)
+                self._to_linear = None
+                self.convs(x)
+
+                self.fc1 = nn.Linear(self._to_linear, 512)
+                self.fc2 = nn.Linear(512, 2)
+
+        def convs(self, x):
+                x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+                x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+                x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
+                
+                #print(x[0].shape)
+                if self._to_linear is None:
+                        self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+                return x
+        
+        def forward(self, x):
+                x = self.convs(x)
+                x = x.view(-1, self._to_linear)
+                x = F.relu(self.fc1(x))
+                x = self.fc2(x)
+                return x
+
+net = Net()
+optimizer = optim.Adam(net.parameters(), lr = 0.001)
+loss_function = nn.MSELoss()
+X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
+X = X/225.0
+y = torch.Tensor([i[1] for i in training_data])
+
+VAL_PCT = 0.1
+val_size = int(len(X)*VAL_PCT)
+#print(val_size)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+#print(len(train_X))
+#print(len(test_X))
+
+BATCH_SIZE = 100
+EPOCHS = 1
+for epoch in range(EPOCHS):
+	for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+		batch_x = train_X[i:i+BATCH_SIZE].view(-1,1 ,50,50)
+		batch_y = train_y[i:i+BATCH_SIZE]
+		net.zero_grad()
+		outputs = net(batch_x)
+		loss = loss_function(outputs, batch_y)
+		loss.backward()
+		optimizer.step()
+
+print(loss)
+correct = 0 
+total = 0
+with torch.no_grad():
+	for i in tqdm(range(len(test_X))):
+		real_class = torch.argmax(test_y[i])
+		net_out = net(test_X[i].view(-1,1,50,50))
+		predicted_class = torch.argmax(net_out)
+		if predicted_class == real_class:
+			correct += 1
+		total += 1
+print("Accuracy: ", round(correct/total, 3))
